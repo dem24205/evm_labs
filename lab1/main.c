b2a79ee52ad9d63b9735246ee96d261f42865077
Elizaveta Dobrovlyanina <e.dobrovlyanina@g.nsu.ru>
2025-09-05 22:24:08 +0700

    format code according to style guide

--- lab1/main.c
+++ lab1/main.c
@@ -1,6 +1,22 @@
+#include "math.h"
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello World!");
-  return 0;
+#define ARG M_PI/6.0
+#define ACCURACY 2000000000
+
+double calc_exp(double arg, int accur) {
+    double result = 1.0;
+    double term = 1.0;
+    for (int n = 0; n < accur-1; ++n) {
+        term *= arg/(n+1);
+        result += term;
+    }
+    return result;
 }
+
+int main(void)
+{
+    double result = calc_exp(ARG, ACCURACY);
+    printf("%.15f\n", result);
+    return 0;
+} 
